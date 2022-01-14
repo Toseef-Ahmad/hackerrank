d1b0fd9503810bb773ae0ca6a66f480af1330a9c
Toseef-Ahmad <ahmadtouseef946@gmail.com>
2022-01-14 16:14:06 +0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
 function towerBreakers(n, m) {
   // Write your code here
-
+  
   const towers = new Array(n).fill(m);
-  console.log(towers);
+  console.log(towers)
+  const completedMoves = [];
   const playerOne = towers[Math.floor(Math.random() * n)];
-  console.log(playerOne);
+  const playerTwo = towers[Math.floor(Math.random() * n)];
+  console.log(playerOne)
   const genratePossibleMoves = (value, height) => {
-    const moves = [];
-    console.log(value);
-    for (let i = value - 1; i > 0; i--) {
-      const ttry = value - i;
-      console.log(ttry);
-      console.log(value - ttry);
-
-      if (value % ttry === 0) {
-        moves.push(ttry);
-        console.log(ttry);
+      const moves = [];
+      console.log(value);
+      for (let i = value - 1; i > 0; i--) {
+          const ttry = value - i;
+          console.log(ttry, ' first');
+          console.log(value - ttry);
+          
+          if (value % (ttry) === 0) {
+              moves.push(value - ttry);
+              console.log(value - ttry)
+          }
       }
-    }
-
-    console.log(moves);
-  };
-
-  if (m % 2 === 0) {
-    const playerOneSelectedTower = playerOne;
-    console.log(playerOneSelectedTower, m);
-    if (playerOneSelectedTower % 2 === 0) {
-      genratePossibleMoves(playerOneSelectedTower, m);
-    }
+      
+      return moves;
   }
-  genratePossibleMoves(7, 7);
+  
+  if (m % 2 === 0) {
+      const playerOneSelectedTower = playerOne;
+      console.log(playerOneSelectedTower, m);
+      if (playerOneSelectedTower % 2 === 0) {
+          genratePossibleMoves(playerOneSelectedTower, m);
+      }
+  }
+  console.log(genratePossibleMoves(7, 7));
 }
 towerBreakers(1, 7);
